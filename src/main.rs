199908216b7a9f1a57dccc4d2949d65cdea3c916
JD <jonathan.baker@fsgi.com>
2024-06-28 10:22:40 -0500

    make binary a little more useable by allowing passing in cli params for file path and output type

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ use std::fs;
 use oe_schema_parse::{parse_df, write_df, write_fmt_df};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let file_contents = fs::read_to_string("../wyatt.df")?;
+    let args: Vec<String> = std::env::args().collect();
+    let file_contents = fs::read_to_string(&args[1])?;
     let input = file_contents.as_str();
     let results = parse_df(input);
     if let Ok(results) = results {
-        print!("{}", write_fmt_df(&results));
+        if args.len() > 2 && &args[2] == "-json" {
+           print!("{}", serde_json::to_string_pretty(&results)?);
+        }
+        else {
+            print!("{}", write_fmt_df(&results));
+        }
     }
 
     Ok(())
